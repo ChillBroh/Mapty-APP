2fc4ca99069f17a866fd06431951cc7d2c310d12
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-01-18 17:10:32 +0530

    pin setting with form done

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, clickEvent;
 //check wheter location available
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
@@ -16,17 +17,17 @@ if (navigator.geolocation) {
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
       //add a marker with a detials in the map usin leaflet
-      var map = L.map('map').setView([latitude, longitude], 13);
+      map = L.map('map').setView([latitude, longitude], 13);
 
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
       //create a new pin on map every setting up of wokout schedule
-      map.on('click', function (clickEvent) {
-        const { lat, lng } = clickEvent.latlng;
-
-        L.marker([lat, lng]).addTo(map).bindPopup('Workout').openPopup();
+      map.on('click', function (click) {
+        clickEvent = click;
+        form.classList.remove('hidden');
+        inputDistance.focus();
       });
     },
     //set default value if the getting location falils
@@ -34,4 +35,32 @@ if (navigator.geolocation) {
       alert("Couldn't get your current position");
     }
   );
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    //clear values after submit
+    inputCadence.value =
+      inputDistance.value =
+      inputDuration.value =
+      inputElevation.value =
+        '';
+    //display marker
+    console.log(clickEvent);
+    const { lat, lng } = clickEvent.latlng;
+
+    L.marker([lat, lng])
+      .addTo(map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+  });
 }
